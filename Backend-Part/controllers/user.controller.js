dd7c891f97ba77b7a00dc4ca8de848996b463a72
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-09-02 15:45:31 +0530

    Creating the user route and Controller in Backend

---
+++ Backend-Part/controllers/user.controller.js
@@ -0,0 +1,6 @@
+async function userController(req,res,next) {
+    return res.json({
+        message : "Api is Working"
+    })
+}
+export default userController;
--- Backend-Part/index.js
+++ Backend-Part/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { PORT } from './configuration/serverConfig.js';
 import connectDb from './configuration/dbConfig.js';
-
+import userRouter from './routes/user.router.js'
 const app = express();
 
+app.use('/api/user',userRouter);
 app.listen(PORT,async ()=>{
     await connectDb();
     console.log(`Server is started at ${PORT}`);
