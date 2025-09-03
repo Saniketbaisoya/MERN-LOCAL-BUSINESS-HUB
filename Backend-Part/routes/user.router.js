bfdfd284df4ff7f695cbc535830788d4c4dd4f78
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-09-03 18:01:24 +0530

    Creating the functionality in sign Up

--- Backend-Part/routes/user.router.js
+++ Backend-Part/routes/user.router.js
@@ -3,6 +3,9 @@ import userController from '../controllers/user.controller.js';
 
 const router = express.Router();
 
+/**
+ * http://localhost:9000/api/user/test
+ */
 router.get('/test',userController);
 
 export default router
--- Local-Buisness-Hub/src/pages/signUp.jsx
+++ Local-Buisness-Hub/src/pages/signUp.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 /**
  * SignUp means login yani user iss component ke upr login krega.
  * With the given details (userName,email and password)
@@ -8,22 +8,73 @@ import {Link} from 'react-router-dom'
  * Uske pass yeah data jayega....
  */
 export default function SignUp() {
+  const [formData,setFormData] = useState({});
+  const navigate = useNavigate();
+  const [error,setError] = useState(null);
+  const[loading,setLoading] = useState(false);
+
+
+  const handleChange = (e) =>{
+    console.log("changing:", e.target.id, "=", e.target.value);
+    setFormData({
+      ...formData,
+      [e.target.id] : e.target.value
+      
+    });
+  };
+  console.log(formData);
+
+  /**
+   * Now this event handler is the function of the signUp button 
+   * Means when we click to the button then this handleSubmit eventHandler should be executed 
+   * Then this will track the whole action(which perform by the client on button component) and send to the backend userRouter api
+   * Then the user will be created...
+   * 
+   */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Now iss yha proxy ka use krke jo authRouter(backend) pr request raise kr rhe hai
+      // isme proxy ka methond kya hoga usko alg se req ki jgh define krte hai
+      // also yha req data ka type kaisa hoga voh bhi dege...
+      setLoading(true);
+      const res = await fetch('/api/',{
+        method : 'POST',
+        headers : {
+          'Content-Type' : 'application/json',
+        },
+        body : JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if(data.success == false){
+        setLoading(false)
+        setError(data.message);
+      }
+      navigate('/signIn');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  console.log(formData);
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-        <form className='flex flex-col gap-4 font-semibold '>
+
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4 font-semibold '>
           <input type="text" placeholder='username'
-            className='border p-3 rounded-lg shadow-2xl' id="username"
+            className='border p-3 rounded-lg shadow-2xl' id="userName" onChange={handleChange}
           />
           <input type="email" placeholder='email'
-            className='border p-3 rounded-lg shadow-2xl' id="email"
+            className='border p-3 rounded-lg shadow-2xl' id="email" onChange={handleChange}
           />
           <input type="password" placeholder='password'
-            className='border p-3 rounded-lg shadow-2xl' id="password"
+            className='border p-3 rounded-lg shadow-2xl' id="password" onChange={handleChange}
           />
-          <button className='bg-slate-700 text-white p-3
-          rounded-lg uppercase hover:bg-opacity-95 disabled:opacity-80 shadow-2xl' id="username"
-          >Sign Up</button>
+          <button disabled={loading} type='submit' className='bg-slate-700 text-white p-3
+          rounded-lg uppercase hover:bg-opacity-95 disabled:opacity-80 shadow-2xl'
+          >{loading ? 'Loading...' : 'Sign Up'}</button>
         </form>
         <div className='flex gap-1 mt-2'>
           <p>have an account?</p>
