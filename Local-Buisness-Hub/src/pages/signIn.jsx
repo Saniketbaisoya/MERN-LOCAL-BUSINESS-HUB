f42bebf96e5f3954f3785f10c488b320d9037116
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-09-04 16:01:52 +0530

    Creating the signIn page and proper add functionality with backend signInRouter

--- Local-Buisness-Hub/src/pages/signIn.jsx
+++ Local-Buisness-Hub/src/pages/signIn.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+/**
+ * SignUp means login yani user iss component ke upr login krega.
+ * With the given details (userName,email and password)
+ * Then yeah data user enter krega client side pr,
+ * Then yeah data phr backend ke through kri hui api userRouter vali expose 
+ * Uske pass yeah data jayega....
+ */
+export default function SignUp() {
+  const [formData,setFormData] = useState({});
+  const navigate = useNavigate();
+  const [error,setError] = useState(null);
+  const[loading,setLoading] = useState(false);
 
-export default function SignIn() {
+
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData,
+      [e.target.id] : e.target.value
+      
+    });
+  };
+  console.log(formData);
+
+  /**
+   * Now this event handler is the function of the signIp button 
+   * Means when we click to the button then this handleSubmit eventHandler should be executed 
+   * Then this will track the whole action(which perform by the client on button component) and send to the backend authRouter api
+   * Then the user will be created...
+   * 
+   */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Now iss yha proxy ka use krke jo authRouter(backend) pr request raise kr rhe hai
+      // isme proxy ka methond kya hoga usko alg se req ki jgh define krte hai
+      // also yha req data ka type kaisa hoga voh bhi dege...
+      setLoading(true);
+      const res = await fetch('/api/signin',{
+        method : 'POST',
+        headers : {
+          'Content-Type' : 'application/json',
+        },
+        body : JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if(data.success == false){
+        setLoading(false)
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  console.log(formData);
   return (
-    <div>
-      SignIn
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4 font-semibold '>
+          <input type="email" placeholder='email'
+            className='border p-3 rounded-lg shadow-2xl' id="email" onChange={handleChange}
+          />
+          <input type="password" placeholder='password'
+            className='border p-3 rounded-lg shadow-2xl' id="password" onChange={handleChange}
+          />
+          <button disabled={loading} type='submit' className='bg-slate-700 text-white p-3
+          rounded-lg uppercase hover:bg-opacity-95 disabled:opacity-80 shadow-2xl'
+          >{loading ? 'Loading...' : 'Sign In'}</button>
+        </form>
+        <div className='flex gap-1 mt-2'>
+          <p className='font-semibold'> Dont have an account?</p>
+          <Link to="/signUp">
+          <span className='text-blue-700 hover:underline font-semibold hover:opacity-85'>Sign Up</span>
+          </Link>
+          {error && 
+            <div className='p-3 max-w-lg mx-auto'>
+            <p className="inline-block text-center  text-sm font-medium text-red-700 bg-red-100 border border-red-400 rounded-lg px-4 py-2 shadow-md animate-bounce">{error}</p>
+            </div>
+          }
+        </div>
     </div>
   )
 }
