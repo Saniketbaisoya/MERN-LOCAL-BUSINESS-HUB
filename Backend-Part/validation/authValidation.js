423d7bf7f7237deb59c8d1fa0c69ff4c83b9071e
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-09-12 04:12:26 +0530

    fix: fixing the errorHandling using ErrorResponse structure

--- Backend-Part/validation/authValidation.js
+++ Backend-Part/validation/authValidation.js
@@ -5,23 +5,28 @@ import AppError from '../utils/AppError.js';
 import { SECRET_KEY } from '../configuration/serverConfig.js';
 
 async function isLoggedIn(req,res,next) {
-    
     const token = req.cookies['access-token'];
     if(!token){
-        throw new AppError("Unauthorized",StatusCodes.UNAUTHORIZED);
+        // throw new AppError("Unauthorized",StatusCodes.UNAUTHORIZED);
+        ErrorResponse.message = "Unauthorized";
+        return res.status(StatusCodes.UNAUTHORIZED).json(ErrorResponse);
     }
     try {
         const decode = jwt.verify( token, SECRET_KEY );
         if(!decode){
-            throw new AppError("Forbidden",StatusCodes.FORBIDDEN);
+            // throw new AppError("Forbidden",StatusCodes.FORBIDDEN);
+            ErrorResponse.message = "Forbidden";
+            return res.status(StatusCodes.FORBIDDEN).json(ErrorResponse);
         }
         req.user = {
-            id : decode.id,
-            email : decode.email
+            id : decode.id
         }
         next();
     } catch (error) {
-        throw new AppError("Invalid Token",StatusCodes.UNAUTHORIZED);
+        // throw new AppError("Invalid Token",StatusCodes.UNAUTHORIZED);
+        ErrorResponse.message = "Invalid Token";
+        ErrorResponse.error = error
+        return res.status(StatusCodes.UNAUTHORIZED).json(ErrorResponse);
     }
 
 }
