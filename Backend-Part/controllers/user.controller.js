5b27b26873ae30d5e41d8c6a7eaf41ac2c755f80
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-09-12 06:11:53 +0530

    fix : fixing the clearCookie brackets

--- Backend-Part/controllers/user.controller.js
+++ Backend-Part/controllers/user.controller.js
@@ -1,8 +1,10 @@
 import { StatusCodes } from "http-status-codes";
+import bcryptjs from 'bcryptjs'
 import AppError from "../utils/AppError.js";
 import SuccessResponse from "../utils/successResponse.js";
 import ErrorResponse from "../utils/ErrorResponse.js";
-import updateService from "../service/user.service.js";
+import  { deleteService, updateService } from "../service/user.service.js";
+import User from "../schema/user.model.js";
 
 async function userController(req,res,next) {
     return res.json({
@@ -17,6 +19,14 @@ async function updateController(req,res) {
         return res.status(StatusCodes.UNAUTHORIZED).json(ErrorResponse);
     }
     try {
+        const email = await User.findOne({email : req.body.email});
+        if(email){
+            ErrorResponse.message = "This email is already exists !!";
+            return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+        }
+        if(req.body.password){
+            bcryptjs.hashSync(req.body.password,10);
+        }
         const response = await updateService(
             req.params.id,
             {
@@ -24,16 +34,34 @@ async function updateController(req,res) {
                 email : req.body.email,
                 password : req.body.password,
                 avatar : req.body.avatar
-            }
+            },
         );
         SuccessResponse.message = "Successfully updated the data";
         const {password : pass, ...rest} = response._doc;
         SuccessResponse.data = rest;
         return res.status(StatusCodes.OK).json(SuccessResponse);
     } catch (error) {
-        ErrorResponse.message = "Something went wrong can't update the data";
+        ErrorResponse.message = error.message;
         ErrorResponse.error = error;
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
     }
 }
-export  {userController, updateController};
+
+async function deleteController(req,res) {
+    if(req.user.id != req.params.id){
+        ErrorResponse.message = "You can only delete your own account!"
+        return res.status(StatusCodes.UNAUTHORIZED).json(ErrorResponse); 
+    }
+    try {
+        await deleteService(req.params.id);
+        SuccessResponse.message = "Successfully deleted the account !!";
+
+        res.clearCookie('access-token');
+        res.status(StatusCodes.OK).json(SuccessResponse.message);
+    } catch (error) {
+        ErrorResponse.message = error.message;
+        ErrorResponse.error = error;
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
+    }
+}
+export  {userController, updateController, deleteController};
