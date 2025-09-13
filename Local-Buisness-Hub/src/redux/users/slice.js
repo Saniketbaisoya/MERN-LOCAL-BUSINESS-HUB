d2536560cb558b58f0a2d81f84ee8cca6e5d560c
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-09-13 13:53:24 +0530

    fix : fixing the signOutSuccess action state.currentUser is null not action.payload

--- Local-Buisness-Hub/src/redux/users/slice.js
+++ Local-Buisness-Hub/src/redux/users/slice.js
@@ -18,13 +18,61 @@ const userSlice = createSlice({
             state.error = null;
         },
         signInFailure : (state,action)=>{
+            state.loading = false;
+            state.error = action.payload;
+        },
+        updateInStart : (state) =>{
+            state.loading = true;
+        },
+        updateInSuccess : (state, action) =>{
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateInFailure : (state, action) =>{
+            state.loading = false;
+            state.error = action.payload;  
+        },
+        deleteInStart : (state)=>{
+            state.loading = true;
+        },
+        deleteInSuccess : (state) =>{
             state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        deleteInFailure : (state,action) =>{
+            state.loading = false;
+            state.error = action.payload;
+        },
+        signOutStart : (state) => {
+            state.loading = true;
+        },
+        signOutSuccess : (state,action) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = false;
+        },
+        signOutFailure : (state,action) => {
             state.loading = false;
             state.error = action.payload;
         }
     },
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const { 
+    signInStart, 
+    signInSuccess, 
+    signInFailure, 
+    updateInStart, 
+    updateInSuccess, 
+    updateInFailure, 
+    deleteInStart, 
+    deleteInSuccess,
+    deleteInFailure,
+    signOutStart,
+    signOutSuccess,
+    signOutFailure 
+} = userSlice.actions;
 
 export default userSlice.reducer;
