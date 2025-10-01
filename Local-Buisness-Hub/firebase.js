403cc92cd43e5d7eb4c51af9cf69ed3b32c24de9
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-10-01 18:10:16 +0530

    fix : changing the firebase

--- Local-Buisness-Hub/firebase.js
+++ Local-Buisness-Hub/firebase.js
@@ -1,3 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY ,
+//   authDomain: "local-business-hub-cc32d.firebaseapp.com",
+//   projectId: "local-business-hub-cc32d",
+//   storageBucket: "local-business-hub-cc32d.firebasestorage.app",
+//   messagingSenderId: "352290294885",
+//   appId: "1:352290294885:web:4f6711e3e695f1b37e4a8c"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,11 +24,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY ,
-  authDomain: "local-business-hub-cc32d.firebaseapp.com",
-  projectId: "local-business-hub-cc32d",
-  storageBucket: "local-business-hub-cc32d.firebasestorage.app",
-  messagingSenderId: "352290294885",
-  appId: "1:352290294885:web:4f6711e3e695f1b37e4a8c"
+  authDomain: "local-business-hub-556c5.firebaseapp.com",
+  projectId: "local-business-hub-556c5",
+  storageBucket: "local-business-hub-556c5.firebasestorage.app",
+  messagingSenderId: "906794768841",
+  appId: "1:906794768841:web:5efed1c4da2b6faa8e3018"
 };
 
 // Initialize Firebase
