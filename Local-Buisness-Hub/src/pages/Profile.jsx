29e5c6af4ccc066116e0f40a4e671babb61a4811
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-10-01 18:11:06 +0530

    adding image upload functionality in profile page

--- Local-Buisness-Hub/src/pages/Profile.jsx
+++ Local-Buisness-Hub/src/pages/Profile.jsx
@@ -5,9 +5,9 @@ import { useDispatch, useSelector } from 'react-redux'
 import { deleteInFailure, deleteInStart, deleteInSuccess, signOutFailure, signOutStart, signOutSuccess, updateInFailure, updateInStart, updateInSuccess } from '../redux/users/slice.js';
 import { data, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-
+import { app } from '../../firebase.js';
+import {getStorage, uploadBytesResumable, ref, getDownloadURL} from 'firebase/storage'
 export default function Profile() {
-  
   const {currentUser, loading, error} = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [formData,setFormData] = useState({});
@@ -15,7 +15,40 @@ export default function Profile() {
   const [update,setUpdate] = useState(false);
   const [signOut,setSignOut] = useState(false);
   const navigate = useNavigate();
+  const [file,setFile] = useState(undefined);
+  const [filePer,setFilePer] = useState(0);
+  const [fileUploadError,setFileUploadError] = useState(false);
   
+  console.log(filePer);
+  useEffect(()=>{
+    if(file){
+      handleFileUpload(file);
+    }
+  },[file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(progress);
+        setFilePer(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  }
 
   const handleChange = (e) => {
     setFormData({
@@ -101,19 +134,42 @@ export default function Profile() {
       dispatch(signOutFailure(data.message));
     }
   }
+
+  // firebase storage rules apply kiye gye hain....
+  // allow read;
+  // allow write : if 
+  // request.resource.size < 2 * 1024 * 1024 && request.resource.contentType.matches('image/.*')
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
         Profile
       </h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4 font-semibold'>
-        <input type='file' ref={fileRef} hidden accept='image/*'/>
+        <input 
+          onChange={(e)=> setFile(e.target.files[0]) } 
+          type='file' 
+          ref={fileRef} 
+          hidden 
+          accept='image/*'
+        />
         <img 
           onClick={()=> fileRef.current.click()}
           className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' 
           src={ formData.avatar || currentUser.data.avatar } 
           alt='profile'
         />
+        {/* Now abb yha pr filePer ko show krege */}
+        <p className=' font-semibold self-center'>
+          {fileUploadError ? (
+          <span className='text-red-700'>Image Upload Error(image must be less than 2 mb)</span>
+          ) : filePer > 0 && filePer < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePer}%`}</span>
+          ) : filePer === 100 ? (
+            <span className=' text-green-700'>Image successfully uploaded!!</span>
+          ) : (
+            ''
+          )}
+        </p>
         <input 
           onChange={handleChange}
           type='text' 
