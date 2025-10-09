7cbe102934a2530c657ec39f2361b61a38e97ca2
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-10-09 16:51:44 +0530

    Adding the imageUpload Functionality and after upload the image is now listed below

--- Local-Buisness-Hub/src/pages/Create_Listing.jsx
+++ Local-Buisness-Hub/src/pages/Create_Listing.jsx
@@ -1,6 +1,81 @@
-import {React} from 'react'
+// Now kbhi bhi koi eventHandler jb register hota hai kisi bhi component/tag ke andr toh agr uss eventhandler ko call krke usme arguments pass krke usko register krte hai toh {()=> eventHandler()}
+// Other wise if there is no parameter is passing then we can normally define the eventHandler into the tag/component {eventHandler}
+
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { useState } from "react"
+import { app } from "../../firebase";
 
 export default function Create_Listing() {
+  const [files,setFiles] = useState([]);
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading,setUploading] = useState(false);
+  // Now formData yeah jo piece of state hai yeah basically voh sara data list ka store krega jo hmme backend pe proxy req lgate hue bejna hai....
+  // Now abhi intially as we know the schema of listing all the imagesurls is store in imageUrls, initally jbb Promise ke upr jake sare urls ko fetch krke jbb hmm usko store krege setForm data ka use krke formData ke imageUrl mai
+  // isliye hmne abhi form data mai phele imageUrl krke variable bnaya hai phele or iss variable ka name schema ke imageUrls ke name se match hona chahiye otherwise database smj ni payega ki kha store krna hai....
+  const [formData,setFormData] = useState({
+    imageUrls : []
+  })
+  console.log(files);
+  console.log(formData);
+
+
+  const handleImageUpload = (e)=> {
+    if(files.length > 0 && files.length + formData.imageUrls.length < 7){
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+
+      for(let i = 0; i < files.length; i++){
+        promises.push(storageImage(files[i]));
+      }
+
+      Promise.all(promises).then((urls)=>{
+        setFormData({
+          ...formData,
+          imageUrls: formData.imageUrls.concat(urls)
+        });
+        setImageUploadError(false);
+        setUploading(false);
+      }).catch((error)=> {
+        setImageUploadError("Image upload failed (2mb max per image)");
+      });
+    }else{
+      setImageUploadError("You can only upload 6 images per listing");
+      setUploading(false);
+    }
+
+  };
+  const storageImage = async (file)=> {
+    return new Promise((resolve,reject)=> {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on('state_changed',
+        (snapshot)=> {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=> {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls : formData.imageUrls.filter((_,i)=> i != index)
+    });
+  }
+
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Creating a Listing</h1>
@@ -17,7 +92,7 @@ export default function Create_Listing() {
             maxLength = '60'
             required
           />
-          <textarea 
+          <textarea
             type='text'
             placeholder = "Description"
             className=' font-semibold border p-3 rounded-lg'
@@ -167,7 +242,12 @@ export default function Create_Listing() {
             </span>
           </p>
           <div className='flex gap-4'>
+          {/* Now sbse phele koi bhi user jb file ko upload krega
+            * Toh usse hmm kisi piece of state ke andr sbse phele store krege 
+            * Then uss piece of state ka use krke hmm eventHandler jo button mai registered hoga uske andr use krege unn file ko backend mai send krne ke liye
+           */}
             <input
+              onChange={(e)=> setFiles(e.target.files)}
               className='p-3 border font-semibold border-gray-300 rounded w-full'
               type='file'
               id='images'
@@ -176,12 +256,40 @@ export default function Create_Listing() {
             />
             <button
               type='button'
+              disabled={uploading}
+              onClick={handleImageUpload}
               className='p-3 font-semibold text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'
             >
-            Upload
+            {uploading ? "Uploading..." : "Upload"}
             </button>
             
           </div>
+          <div className=" text-red-700  font-semibold text-center">{imageUploadError && imageUploadError}</div>
+          {/* Now abb agr imageUrls ke andr images hai if the length is > 0 then we will list the images below the upload button
+            * also we have a button of delete so that user can direclty delete the image
+            * that is why abb hmm har ek imageUrls ke upr jakr using map sare url ko lekr usko phele list krege and hr ek ke corressponding show krege delete button ko 
+           */}
+          <p>
+            {formData.imageUrls.length > 0 && 
+              formData.imageUrls.map((urls,index)=> (
+                <div key={urls} className='flex justify-between p-3 border items-center'>
+                  <img 
+                    src={urls} 
+                    alt="listing image" 
+                    className="w-20 h-20  object-cover rounded-xl border border-gray-200 hover:scale-105 transition-transform duration-300"
+                  />
+                  <button 
+                    type="button" 
+                    onClick={()=> handleRemoveImage(index)}
+                    // className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75"
+                    className="px-4 py-2 text-sm font-semibold text-white bg-slate-700 rounded-lg hover:bg-slate-500 active:bg-slate-500 transition-all duration-200 shadow-sm hover:shadow-md"
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))
+            }
+          </p>
           <button className='p-3 font-semibold bg-slate-700 text-white rounded-lg uppercase hover:opacity-95'>Create Listing</button>
         </div> 
           
