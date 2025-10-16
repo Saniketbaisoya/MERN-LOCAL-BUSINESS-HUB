cbf8d1a034558cdcd99bd89afae1b7416012674f
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-10-17 05:21:12 +0530

    Adding the show listings in search.jsx or then one by one all listings is shown in listingItems.jsx component

--- Local-Buisness-Hub/src/pages/Search.jsx
+++ Local-Buisness-Hub/src/pages/Search.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
 import {useNavigate} from 'react-router-dom'
+import ListingItem from '../components/ListingItem';
 
 export default function Search() {
     const navigate = useNavigate();
     const [listings,setListings] = useState([]);
+    const [loading,setLoading] = useState(false);
     const [sideBarData,setSideBarData] = useState({
         searchTerm : '',
         type : 'all',
@@ -42,14 +44,17 @@ export default function Search() {
         }
 
         const fetchListings = async () => {
+            setLoading(true);
             const searchQuery = urlParams.toString();
-            const response = await fetch(`/api/listing/get?${searchQuery}`
-            );
+            const response = await fetch(`/api/listing/get?${searchQuery}`,{
+                method : 'GET'
+            });
             const data = await response.json();
             if(data.success == false){
                 console.log(data.message);
             }
             setListings(data.data);
+            setLoading(false);
 
         };
         fetchListings();
@@ -85,127 +90,141 @@ export default function Search() {
         urlParams.set('sort',sideBarData.sort);
         urlParams.set('order',sideBarData.order);
         const searchQuery = urlParams.toString();
-        navigate(`/seacrh/${searchQuery}`);
+        navigate(`/search?${searchQuery}`);
     }
 
     return (
         <div className='flex flex-col md:flex-row'>
-        {/* Now abb yha seacrhterm or listings results ayege.... */}
-        {/*  This div for the form of searchTerm*/}
-        <div className='p-7  border-b-2 md:border-r-2 md:min-h-screen'>
-            <form onSubmit={handleSubmit} className=' flex flex-col gap-8'>
+            {/* Now abb yha seacrhterm or listings results ayege.... */}
+            {/*  This div for the form of searchTerm*/}
+            <div className='p-7  border-b-2 md:border-r-2 md:min-h-screen'>
+                <form onSubmit={handleSubmit} className=' flex flex-col gap-8'>
 
-                {/* Now isme bhi multiple divs hoge for Search Term, for type, for amenities, for sort and for seacrh button */}
-                <div className=' flex items-center gap-2'>
-                    <label className=' whitespace-nowrap rounded-lg p-1.5 text-white bg-slate-500'>Seacrh Term:</label>
-                    <input 
-                        type='text'
-                        id='searchTerm'
-                        placeholder='Search...'
-                        className=' border rounded-lg p-3 w-full bg-slate-100'
-                        onChange={handleChange}
-                        value={sideBarData.searchTerm}
-                    />
-                </div>
+                    {/* Now isme bhi multiple divs hoge for Search Term, for type, for amenities, for sort and for seacrh button */}
+                    <div className=' flex items-center gap-2'>
+                        <label className=' whitespace-nowrap rounded-lg p-1.5 text-white bg-slate-500'>Seacrh Term:</label>
+                        <input 
+                            type='text'
+                            id='searchTerm'
+                            placeholder='Search...'
+                            className=' border rounded-lg p-3 w-full bg-slate-100'
+                            onChange={handleChange}
+                            value={sideBarData.searchTerm}
+                        />
+                    </div>
 
-                {/* Now here i have a div of type where rent&sale, rent, sale present.... */}
-                <div className=' flex gap-2 flex-wrap items-center'>
-                    <label className=' rounded-lg p-1.5 text-white bg-slate-500'>Type:</label>
-                    <div className=' flex gap-2'>
-                        <input 
-                            type='checkbox'
-                            id='all'
-                            className='w-5'
-                            onChange={handleChange}
-                            checked={sideBarData.type === 'all'}
-                            
-                        />
-                        <span className=' font-semibold'>Rent & Sale</span>
+                    {/* Now here i have a div of type where rent&sale, rent, sale present.... */}
+                    <div className=' flex gap-2 flex-wrap items-center'>
+                        <label className=' rounded-lg p-1.5 text-white bg-slate-500'>Type:</label>
+                        <div className=' flex gap-2'>
+                            <input 
+                                type='checkbox'
+                                id='all'
+                                className='w-5'
+                                onChange={handleChange}
+                                checked={sideBarData.type === 'all'}
+                                
+                            />
+                            <span className=' font-semibold'>Rent & Sale</span>
+                        </div>
+                        <div className=' flex gap-2'>
+                            <input 
+                                type='checkbox'
+                                id='rent'
+                                className='w-5'
+                                onChange={handleChange}
+                                checked={sideBarData.type === 'rent'}
+                            />
+                            <span className=' font-semibold'>Rent</span>
+                        </div>
+                        <div className=' flex gap-2'>
+                            <input 
+                                type='checkbox'
+                                id='sale'
+                                className='w-5'
+                                onChange={handleChange}
+                                checked={sideBarData.type === 'sale'}
+                            />
+                            <span className=' font-semibold'>Sale</span>
+                        </div>
+                        <div className=' flex gap-2'>
+                            <input 
+                                type='checkbox'
+                                id='offer'
+                                className='w-5'
+                                onChange={handleChange}
+                                checked={sideBarData.offer}
+                            />
+                            <span className=' font-semibold'>Offer</span>
+                        </div>
                     </div>
-                    <div className=' flex gap-2'>
-                        <input 
-                            type='checkbox'
-                            id='rent'
-                            className='w-5'
-                            onChange={handleChange}
-                            checked={sideBarData.type === 'rent'}
-                        />
-                        <span className=' font-semibold'>Rent</span>
-                    </div>
-                    <div className=' flex gap-2'>
-                        <input 
-                            type='checkbox'
-                            id='sale'
-                            className='w-5'
-                            onChange={handleChange}
-                            checked={sideBarData.type === 'sale'}
-                        />
-                        <span className=' font-semibold'>Sale</span>
-                    </div>
-                    <div className=' flex gap-2'>
-                        <input 
-                            type='checkbox'
-                            id='offer'
-                            className='w-5'
-                            onChange={handleChange}
-                            checked={sideBarData.offer}
-                        />
-                        <span className=' font-semibold'>Offer</span>
-                    </div>
-                </div>
 
-                {/* Now here i have a div of Amenities in which parkinga and furnished here.... */}
-                <div className=' flex gap-2 flex-wrap items-center'>
-                    <label className=' rounded-lg p-1.5 text-white bg-slate-500'>Amenities:</label>
-                    <div className=' flex gap-2'>
-                        <input 
-                            type='checkbox'
-                            id='parking'
-                            className='w-5'
-                            onChange={handleChange}
-                            checked={sideBarData.parking}
-                        />
-                        <span className=' font-semibold'>Parking</span>
+                    {/* Now here i have a div of Amenities in which parkinga and furnished here.... */}
+                    <div className=' flex gap-2 flex-wrap items-center'>
+                        <label className=' rounded-lg p-1.5 text-white bg-slate-500'>Amenities:</label>
+                        <div className=' flex gap-2'>
+                            <input 
+                                type='checkbox'
+                                id='parking'
+                                className='w-5'
+                                onChange={handleChange}
+                                checked={sideBarData.parking}
+                            />
+                            <span className=' font-semibold'>Parking</span>
+                        </div>
+                        <div className=' flex gap-2'>
+                            <input 
+                                type='checkbox'
+                                id='furnished'
+                                className='w-5'
+                                onChange={handleChange}
+                                checked={sideBarData.furnished}
+                            />
+                            <span className=' font-semibold'>Furnished</span>
+                        </div>
                     </div>
-                    <div className=' flex gap-2'>
-                        <input 
-                            type='checkbox'
-                            id='furnished'
-                            className='w-5'
-                            onChange={handleChange}
-                            checked={sideBarData.furnished}
-                        />
-                        <span className=' font-semibold'>Furnished</span>
-                    </div>
-                </div>
 
-                {/* Now here i have a sort option where use can list in order to highToLow, lowToHigh, latest, oldest.... */}
-                <div className=' flex items-center gap-2'>
-                    <label className=' rounded-lg p-1.5 text-white bg-slate-500'>Sort:</label>
-                    <select
-                        onChange={handleChange}
-                        className=' border rounded-lg p-3 font-semibold'
-                        id='sort_order'
-                        defaultValue={'created_at_desc'}
+                    {/* Now here i have a sort option where use can list in order to highToLow, lowToHigh, latest, oldest.... */}
+                    <div className=' flex items-center gap-2'>
+                        <label className=' rounded-lg p-1.5 text-white bg-slate-500'>Sort:</label>
+                        <select
+                            onChange={handleChange}
+                            className=' border rounded-lg p-3 font-semibold'
+                            id='sort_order'
+                            defaultValue={'created_at_desc'}
+                        >
+                            <option>Price high to low</option>
+                            <option>Price low to high</option>
+                            <option>Latest</option>
+                            <option>Oldest</option>
+                        </select>
+                    </div>
+                    {/*  Here we have a seacrh button....*/}
+                    <button
+                        className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
                     >
-                        <option>Price high to low</option>
-                        <option>Price low to high</option>
-                        <option>Latest</option>
-                        <option>Oldest</option>
-                    </select>
+                        Search
+                    </button>
+                </form>
+            </div>
+            {/* This div for the listings results */}
+            <div>
+                <h1 className=' text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing results:</h1>
+                
+                <div className='p-7 flex flex-wrap gap-4'>
+                    {!loading && listings.length == 0 && (
+                        <p className='text-xl text-slate-700'>No Listing Found !!</p>
+                    )}
+                    {loading && (
+                        <p className='text-xl text-slate-700 text-center w-full'>Loading....</p>
+                    )}
+                    {!loading && listings && 
+                        listings.map((listing)=> (
+                            <ListingItem key={listing._id} listings={listing}/>
+                        ))
+                    }
                 </div>
-                {/*  Here we have a seacrh button....*/}
-                <button
-                    className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
-                >
-                    Search
-                </button>
-            </form>
-        </div>
-        {/* This div for the listings results */}
-        <div>
-            <h1 className=' text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing results:</h1>
-        </div>
+            </div>
         </div>
     )
 }
