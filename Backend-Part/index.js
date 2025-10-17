e233e8a8e7c1ebb1b23725beba3638698d9fb217
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-10-17 18:25:40 +0530

    implementing build command

--- Backend-Part/index.js
+++ Backend-Part/index.js
@@ -5,6 +5,9 @@ import userRouter from './routes/user.router.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js'
+import path from 'path';
+
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}))
@@ -13,6 +16,12 @@ app.use(cookieParser());
 app.use('/api/user',userRouter);
 app.use('/api',authRouter);
 app.use('/api/listing',listingRouter);
+
+app.use(express.static(path.join(__dirname, '/Local-Buisness-Hub/dist')));
+
+app.get('*',(req,res)=> {
+    res.sendFile(path.join(__dirname, 'Local-Buisness-Hub', 'dist', 'index.html'))
+})
 app.listen(PORT,async ()=>{
     await connectDb();
     console.log(`Server is started at ${PORT}`);
